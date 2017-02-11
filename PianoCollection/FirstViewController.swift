9a573b7971d3530686f693fcf2e838fba83324b8
BlueYang <77963964@qq.com>
2017-02-11 22:22:49 +0800

    Initial Commit

---
+++ PianoCollection/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  PianoCollection
+//
+//  Created by  BlueYang on 17/2/11.
+//  Copyright © 2017年  BlueYang. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
